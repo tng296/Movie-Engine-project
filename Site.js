325fd3581c7ff4bb42583a34169ee8197115a88a
Luinceanera <38448169+Luinceanera@users.noreply.github.com>
2023-11-07 14:46:05 -0600

    Create Site.js
    To make it easier to use ID, Name, ClassName, etc.

---
+++ Site.js
@@ -0,0 +1,15 @@
+var $ID = function(id)
+{
+  return document.getElementById(id);
+}
+
+var $NAME = function(name)
+{
+  return document.getElementsByName(name);
+}
+
+var $CLASS = function(className)
+{
+  return document.getElementsByClassName(className);
+}
+
