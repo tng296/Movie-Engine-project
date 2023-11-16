a048cc05215ec12bf34f71c7cac9a94b07ae74f3
markeuli <34800211+markeuli@users.noreply.github.com>
2023-11-16 02:54:10 -0600

    Added formatting and merged work done by others

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
+let intervalId;
+let thumbnails = [
+    "images/movie/movie1.jpg",
+    "images/movie/movie2.jpg",
+    "images/movie/movie3.jpg",
+    "images/movie/movie4.jpg",
+    "images/movie/movie5.jpg",
+];
+let thumbnailQueue = [
+    "images/movie/movie6.jpg",
+    "images/movie/movie7.jpg",
+    "images/movie/movie8.jpg",
+    "images/movie/movie9.jpg",
+    "images/movie/movie10.jpg",
+];
+let firstTB = thumbnails[0];
+let lastTB = thumbnails[thumbnails.length - 1];
+
 function openTab(evt, tabName) {
     
     var i, tabcontent, tablinks;
@@ -15,10 +33,53 @@ function openTab(evt, tabName) {
     }
 
     
-    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).style.display = "flex";
     evt.currentTarget.className += " active";
+
+    if (tabName === 'Browse') {
+        // Simulate a click on the scroll buttons, we do this because the images are not loaded yet
+        document.querySelector('.leftscrollbuttonstyle img').click();
+        document.querySelector('.rightscrollbuttonstyle img').click();
+    }
 }
 
-document.getElementById("Home").style.display = "block";
-document.getElementById("defaultOpen").className += " active";
+function dynamicScroll() {
+    displayThumbnails();
+    intervalId = setInterval(() => {
+        thumbnailQueue.push(firstTB);
+        thumbnails.shift();
+        thumbnails.push(thumbnailQueue[0]);
+        thumbnailQueue.shift();
+        firstTB = thumbnails[0];
+        lastTB = thumbnails[thumbnails.length - 1];
+        displayThumbnails();
+    }, 3000);
+}
+
+function displayThumbnails() {
+    let thumbnailsContainer = document.getElementById("thumbnails-container");
+    thumbnailsContainer.innerHTML = thumbnails
+        .map((thumbnail) => `<img src="${thumbnail}" />`)
+        .join("");
+}
+
+function leftScroll() {
+    clearInterval(intervalId);
+    thumbnailQueue.push(thumbnails.shift());
+    thumbnails.push(thumbnailQueue.shift());
+    displayThumbnails();
+}
+
+function rightScroll() {
+    clearInterval(intervalId);
+    thumbnailQueue.unshift(thumbnails.pop());
+    thumbnails.unshift(thumbnailQueue.pop());
+    displayThumbnails();
+}
+
+window.onload = function() {
+    document.getElementById("Home").style.display = "flex";
+    document.getElementById("defaultOpen").className += " active";
+    dynamicScroll();
+};
 
