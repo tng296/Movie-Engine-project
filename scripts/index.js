80d473b633773d294669b933ecbb09491940dfa5
sam156123328 <sam18328578207@gmail.com>
2023-11-17 13:11:50 -0600

    add personal webpage, and add automatic scroll on script

--- scripts/index.js
+++ scripts/index.js
@@ -17,26 +17,28 @@ let firstTB = thumbnails[0];
 let lastTB = thumbnails[thumbnails.length - 1];
 
 function openTab(evt, tabName) {
-    
+
     var i, tabcontent, tablinks;
 
-    
+
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    
+
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    
+
     document.getElementById(tabName).style.display = "flex";
     evt.currentTarget.className += " active";
 
     if (tabName === 'Browse') {
+        //run this function for automatic scroll from right to left, no need to click
+        dynamicScroll();
         // Simulate a click on the scroll buttons, we do this because the images are not loaded yet
         document.querySelector('.leftscrollbuttonstyle img').click();
         document.querySelector('.rightscrollbuttonstyle img').click();
@@ -77,9 +79,9 @@ function rightScroll() {
     displayThumbnails();
 }
 
-window.onload = function() {
+window.onload = function () {
     document.getElementById("Home").style.display = "flex";
     document.getElementById("defaultOpen").className += " active";
-    dynamicScroll();
+
 };
 
