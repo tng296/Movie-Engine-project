aa91193e8886aefda55b56defebe08e009a67113
sam156123328 <sam18328578207@gmail.com>
2023-11-25 16:48:30 -0600

    update some css js

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,37 @@
 let intervalId;
 let thumbnails = [
-    "images/movie/movie1.jpg",
-    "images/movie/movie2.jpg",
-    "images/movie/movie3.jpg",
-    "images/movie/movie4.jpg",
-    "images/movie/movie5.jpg",
+    "images/movie/Batman.jpg",
+    "images/movie/Dark.jpg",
+    "images/movie/Freedom.jpg",
+    "images/movie/Wood.jpg",
+    "images/movie/Alone.jpg",
 ];
 let thumbnailQueue = [
-    "images/movie/movie6.jpg",
-    "images/movie/movie7.jpg",
-    "images/movie/movie8.jpg",
-    "images/movie/movie9.jpg",
-    "images/movie/movie10.jpg",
+    "images/movie/Army of The Dead.jpg",
+    "images/movie/Judge.jpg",
+    "images/movie/Adam Driver.jpg",
+    "images/movie/Escape Room.jpg",
+    "images/movie/BloodShot.jpg",
 ];
+
+
+
 let firstTB = thumbnails[0];
 let lastTB = thumbnails[thumbnails.length - 1];
 
-function openTab(evt, tabName)
-{
+function openTab(evt, tabName) {
 
     var i, tabcontent, tablinks;
 
 
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++)
-    {
+    for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
 
     tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++)
-    {
+    for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
@@ -39,8 +39,7 @@ function openTab(evt, tabName)
     document.getElementById(tabName).style.display = "flex";
     evt.currentTarget.className += " active";
 
-    if (tabName === 'Browse')
-    {
+    if (tabName === 'Browse') {
         //run this function for automatic scroll from right to left, no need to click
         dynamicScroll();
         // Simulate a click on the scroll buttons, we do this because the images are not loaded yet
@@ -48,15 +47,15 @@ function openTab(evt, tabName)
         document.querySelector('.rightscrollbuttonstyle img').click();
     }
 }
-
-function dynamicScroll()
-{
+function dynamicScroll() {
     displayThumbnails();
-    intervalId = setInterval(() =>
-    {
+    intervalId = setInterval(() => {
+
         thumbnailQueue.push(firstTB);
         thumbnails.shift();
+        thumbnailContent.shift();
         thumbnails.push(thumbnailQueue[0]);
+        thumbnailContent.push(thumbnailmap[thumbnailQueue[0]]);
         thumbnailQueue.shift();
         firstTB = thumbnails[0];
         lastTB = thumbnails[thumbnails.length - 1];
@@ -64,32 +63,39 @@ function dynamicScroll()
     }, 3000);
 }
 
-function displayThumbnails()
-{
-    let thumbnailsContainer = document.getElementById("thumbnails-container");
-    thumbnailsContainer.innerHTML = thumbnails
-        .map((thumbnail) => `<img src="${thumbnail}" />`)
-        .join("");
+function getImageTitle(filePath) {
+    let filename = filePath.split('/').pop().split('.').slice(0, -1).join('.');
+    return '    ' + filename;
 }
 
-function leftScroll()
-{
+function displayThumbnails() {
+    let thumbnailsContainer = document.getElementById("thumbnails-container");
+    thumbnailsContainer.innerHTML = thumbnails
+        .map((thumbnail) => `
+            <div class="thumbnail-item">
+                <img src="${thumbnail}" alt="${getImageTitle(thumbnail)}"/>
+                <h5 class="thumbnail title">  ${getImageTitle(thumbnail)}</h5>
+            </div>
+        `)
+        .join(""); // Join the array of strings to form a single string
+}
+
+
+function leftScroll() {
     clearInterval(intervalId);
     thumbnailQueue.push(thumbnails.shift());
     thumbnails.push(thumbnailQueue.shift());
     displayThumbnails();
 }
 
-function rightScroll()
-{
+function rightScroll() {
     clearInterval(intervalId);
     thumbnailQueue.unshift(thumbnails.pop());
     thumbnails.unshift(thumbnailQueue.pop());
     displayThumbnails();
 }
 
-window.onload = function ()
-{
+window.onload = function () {
     document.getElementById("Home").style.display = "flex";
     document.getElementById("defaultOpen").className += " active";
 };
